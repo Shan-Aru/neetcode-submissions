e6fc744dd791b6879836576b49821608f33fe0a4
Shandhiya <shandhiya000@gmail.com>
2026-08-19 08:30:24 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        for(int num=0;num<(1<<n);num++)
+        {
+            vector<int>temp;
+            for(int i=0;i<n;i++)
+            {
+                if(num &(1<<i))
+                {
+                    temp.push_back(nums[i]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
