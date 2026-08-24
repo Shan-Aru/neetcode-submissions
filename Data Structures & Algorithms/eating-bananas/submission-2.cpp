a100b37568501934ed2d851c29680af62876796c
Shandhiya <shandhiya000@gmail.com>
2026-08-24 17:53:47 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    long long totalhours(vector<int>&piles,int speed){
+        long long hours=0;
+        for (int i=0;i<piles.size();i++)
+        {
+            hours+=(piles[i]+speed-1)/speed;
+        }
+        return hours;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        while(low<high){
+            int mid=(low+high)/2;
+            long long hours=totalhours(piles,mid);
+            if(hours<=h)high=mid;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
